d301e7e1467ff576227fc03e23ebf358ea2b6ca9
Ankitgif <ankitsinha2303@gmail.com>
2020-06-23 03:06:21 +0530

    [Ankit] Async-Await

--- AsyncAwait/AsyncAwait1.js
+++ AsyncAwait/AsyncAwait1.js
@@ -19,9 +19,12 @@ const calculate = (a, b, c) => {
 //     console.log(e);
 // })
 
-const add = async () => {       // (o/p is Promise {undefined} it means async behind the scene behave like a promise  so it is expecting 
-                                //something promise to be returned from within this function)
-return await calculate(1,2,3);      // Promise {pending}  (pending means that async actually this (line) return something or return a value)
-}                              //before the async could wait. async want some body that we can call await to actually notified the async keyword that something has been calculated and you have to wait for that much time.     
-
-console.log(add());
+const add = async () => {                        // (o/p is Promise {undefined} it means async behind the scene behave like a promise  so it is expecting 
+                                                //something promise to be returned from within this function)
+    const sum1 = await calculate(1,2,3);        // Promise {pending}  (pending means that async actually this (line) return something or return a value)
+    const sum2 = await calculate(sum1,4,5);                                                //before the async could wait. async want some body that we can call await to actually notified the async keyword that something has been calculated and you have to wait for that much time.     
+    const sum3 = await calculate(sum2,6,7);
+    const sum4 = await calculate(sum3,8,9);
+    return sum4;
+}
+add().then((value) => console.log(value)).catch((e) => console.log(e));
