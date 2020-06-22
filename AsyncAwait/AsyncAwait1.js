403a76820f6ca42544e53291f543d8106d6ba3bf
Ankitgif <ankitsinha2303@gmail.com>
2020-06-23 02:49:49 +0530

    [Ankit] Promise vs Async-Await(o/p is Promise {undefined} it means async behind the scene behave like a promise  so it is expecting something promise to be returned from within this function)

--- AsyncAwait/AsyncAwait1.js
+++ AsyncAwait/AsyncAwait1.js
@@ -1,20 +1,26 @@
 //Async-Await vs Promises
 
-const calculate = (a, b, c) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(a<0 || b<0 || c<0)
-                return reject('No number can be negative');
-            resolve(a+b+c);    
-        }, 1000)
-    })
+// const calculate = (a, b, c) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(a<0 || b<0 || c<0)
+//                 return reject('No number can be negative');
+//             resolve(a+b+c);    
+//         }, 1000)
+//     })
+// }
+
+// calculate(1, 2, 3).then((addition) => {
+//     console.log(addition);
+//     return calculate(addition, 4, 5)
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+const add = async () => {
+
 }
 
-calculate(1, 2, 3).then((addition) => {
-    console.log(addition);
-    return calculate(addition, 4, 5)
-}).then((result) => {
-    console.log(result);
-}).catch((e) => {
-    console.log(e);
-})
+console.log(add());
