25775fc4a03bce79021d59d98f4af03806728fa5
Ankitgif <ankitsinha2303@gmail.com>
2020-06-22 21:19:01 +0530

    [Ankit] Added Function in javascript(es5 & es6)

---
+++ Function/ArrowFunction.js
@@ -0,0 +1,19 @@
+//ES6 features Arrow function
+
+const sum = () =>{
+    let a = 5;
+    let b = 10;
+    return a+b;
+}
+console.log(sum());
+
+
+
+    let c = 5;
+    let d = 10;
+const mul = () => c*d
+console.log(mul());
+
+
+const add = (e,f) => e+f
+console.log(add(4,5));
---
+++ Function/FunctionES5.js
@@ -0,0 +1,12 @@
+var sum = function() {
+    var a = 5;
+    var b = 10;
+    return a+b;
+}
+console.log(sum());
+
+const square = function(number) { 
+    return number * number 
+}
+var x = square(4)
+console.log(x);
